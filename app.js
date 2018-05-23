30956fd6bd35b4064e0eada090cc415e21f72929
Riza Fahmi <rizafahmi@gmail.com>
2018-05-23 13:13:48 +0700

    Finish upload file on express side

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const fileUpload = require('express-fileupload')
+
 const app = express()
 
 app.use(express.static('public'))
-app.post('/upload', )
+app.use(fileUpload())
+
+app.post('/upload', (req, res) => {
+  if (!req.files)
+    res.statu(400).send('No files were uploaded.')
+  const { picture } = req.files
+  res.json({name: picture.name})
+})
 app.listen(3000)
