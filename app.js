5e39bd4ac19cc4250732da79e771007f8dc9c535
Riza Fahmi <rizafahmi@gmail.com>
2018-05-31 12:35:49 +0700

    Finish hotdog or not hotdog app with AWS Rekognition

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
-const express = require('express')
-const fileUpload = require('express-fileupload')
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const path = require('path');
 
-const recognize = require('./helpers.js')
+const recognize = require('./helpers.js');
 
-const app = express()
+const app = express();
 
-app.use(express.static('public'))
-app.use(fileUpload())
+app.use(express.static('public'));
+app.use(fileUpload());
+
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/upload', (req, res) => {
+  res.render('result');
+});
 
 app.post('/upload', async (req, res) => {
-  if (!req.files)
-    res.statu(400).send('No files were uploaded.')
+  if (!req.files) { res.status(400).send('No files were uploaded.'); }
   try {
-    const {s3Object, labels} = await recognize(req.files.picture)
-    // res.json({
-    //   status: 'OK',
-    //   labels,
-    //   s3Object
-    // })
-    res.send("<img src='https://s3.amazonaws.com/rekog-techinasia/Screen+Shot+2018-05-22+at+10.23.38.png' />")
+    const { s3Object, labels } = await recognize(req.files.picture);
+    const isHotDog = labels.filter(label => label === 'Hotdog' || label === 'Hot Dog').length > 0;
+    res.render('result', { s3Object, labels, isHotDog });
   } catch (err) {
     res.json({
       status: 'KO',
-      error: err
-    })
+      error: err,
+    });
   }
-})
-app.listen(3000)
+});
+app.listen(3000);
--- helpers.js
+++ helpers.js
@@ -1,44 +1,36 @@
-const AWS = require('aws-sdk')
-require('dotenv').config()
+const AWS = require('aws-sdk');
+require('dotenv').config();
 
-const recognize = async ( image ) => {
-  AWS.config.loadFromPath('./.secret.json')
-  const s3Object = await uploadImage("rekog-techinasia", image)
+const recognize = async (image) => {
+  AWS.config.loadFromPath('./.secret.json');
+  const s3Object = await uploadImage('rekog-techinasia', image);
   return new Promise((resolve, reject) => {
-    const rekog = new AWS.Rekognition()
+    const rekog = new AWS.Rekognition();
     const params = {
-      CollectionId: "rekog-techinasia",
-      Image: {
-        Bucket: "rekog-techinasia",
-        Name: image.name
-      }
-    }
-    rekog.detectLabels({
       Image: {
         S3Object: {
-          Bucket: "rekog-techinasia",
-          Name: image.name
-        }
+          Bucket: 'rekog-techinasia',
+          Name: image.name,
+        },
       },
       MaxLabels: 24,
-      MinConfidence: 60
-    }, (err, data) => {
+      MinConfidence: 60,
+    };
+    rekog.detectLabels(params, (err, data) => {
       if (err) {
-        console.error(err)
-        reject(err)
-        return
+        console.error(err);
+        reject(err);
       } else {
-        const labels = data.Labels.map(l => l.Name)
-        console.log(`${image.name}: ${labels.join(", ")}`)
-        resolve({s3Object, labels})
+        const labels = data.Labels.map(l => l.Name);
+        console.log(`${image.name}: ${labels.join(', ')}`);
+        resolve({ s3Object, labels });
       }
-    })
-
-  })
-}
+    });
+  });
+};
 
 const uploadImage = (bucketName, image) => {
-  const s3 = new AWS.S3()
+  const s3 = new AWS.S3();
   return new Promise((resolve, reject) => {
     s3.upload({
       Bucket: bucketName,
@@ -46,20 +38,18 @@ const uploadImage = (bucketName, image) => {
       Body: new Buffer(image.data.buffer),
       ACL: 'public-read',
       Metadata: {
-        'Content-Type': image.mimetype
-      }
+        'Content-Type': image.mimetype,
+      },
     }, (err, data) => {
       if (err) {
-        console.error(err)
-        reject(err)
-        return
+        console.error(err);
+        reject(err);
       } else {
-        console.log(`Uploaded ${image.name} to bucket ${bucketName}`)
-        resolve(data)
+        console.log(`Uploaded ${image.name} to bucket ${bucketName}`);
+        resolve(data);
       }
-    })
+    });
+  });
+};
 
-  })
-}
-
-module.exports = recognize
+module.exports = recognize;
